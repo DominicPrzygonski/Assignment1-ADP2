ff07967178bfe16a5591ad53e6fe11a8a0f6f0b0
Dominic Przygonski <domprzygonski@gmail.com>
2021-03-27 18:47:32 +0200

    DominicPrzygonski_219206414 work commited

--- src/main/java/za/ac/cput/dominicprzygonski_219206414/Example.java
+++ src/main/java/za/ac/cput/dominicprzygonski_219206414/Example.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package za.ac.cput.dominicprzygonski_219206414;
 
 /**
  *
- * @author Dom
+ * @author Dominic Przygonski - 219206414
  */
 public class Example {
+    
+    private String name;
+    private int value1, value2;
+    
+    public int LessThanTen (int first, int second){
+        return first + second;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue1() {
+        return value1;
+    }
+
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+    
+    @Override
+    public String toString() {
+        return "AssingmentClass{" + "name=" + name + ", value1=" + value1 + ", value2=" + value2 + '}';
+    }
     
 }
--- src/test/java/za/ac/cput/dominicprzygonski_219206414/ExampleTest.java
+++ src/test/java/za/ac/cput/dominicprzygonski_219206414/ExampleTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -18,11 +19,18 @@ import static org.junit.Assert.*;
  */
 public class ExampleTest {
     
+    private Example testing1;
+    private Example testing2;
+    private Example testing3;
+    
     public ExampleTest() {
+        
     }
     
     @BeforeClass
     public static void setUpClass() {
+        
+       
     }
     
     @AfterClass
@@ -31,10 +39,53 @@ public class ExampleTest {
     
     @Before
     public void setUp() {
+        testing1 = new Example();
+        testing1.setValue1(5);
+        testing1.setValue2(7);
+        
+        testing2 = new Example();
+        testing2.setValue1(7);
+        testing2.setValue2(5);
+        
+        testing3 = testing2;
     }
     
     @After
     public void tearDown() {
+    }
+
+    //Test Object Equality, meant to pass
+    @Test
+    public void testEquality(){
+        assertEquals(testing1.getValue1(), testing2.getValue2());
+    }
+    
+    //Test Object identity, meant to pass
+    @Test
+    public void testIdentity(){
+        assertSame(testing2, testing3);
+    }
+    
+    //Test fail, meant to fail
+    @Test
+    public void testFail(){
+        fail("This is meant ot fail");
+        assertSame(testing2, testing3);
+    }
+    
+    //Test timeout, meant to fail/error
+    @Test (timeout=2000)
+    public void testTimeout(){
+        while(true){
+            
+        }
+    }
+    
+    //Test disabling, meant to be skip
+    @Test
+    @Ignore
+    public void testIgnore(){
+        System.out.println("This will be skipped");
     }
 
     
