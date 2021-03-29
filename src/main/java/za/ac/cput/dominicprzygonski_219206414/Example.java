2768cd1b96e0a43dc5d625e597779cba49d6c948
Dominic Przygonski <domprzygonski@gmail.com>
2021-03-29 14:54:55 +0200

    Fixing Junit

--- src/main/java/za/ac/cput/dominicprzygonski_219206414/Example.java
+++ src/main/java/za/ac/cput/dominicprzygonski_219206414/Example.java
@@ -6,40 +6,6 @@ package za.ac.cput.dominicprzygonski_219206414;
  */
 public class Example {
     
-    private String name;
-    private int value1, value2;
-    
-    public int LessThanTen (int first, int second){
-        return first + second;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getValue1() {
-        return value1;
-    }
-
-    public void setValue1(int value1) {
-        this.value1 = value1;
-    }
-
-    public int getValue2() {
-        return value2;
-    }
-
-    public void setValue2(int value2) {
-        this.value2 = value2;
-    }
-    
-    @Override
-    public String toString() {
-        return "AssingmentClass{" + "name=" + name + ", value1=" + value1 + ", value2=" + value2 + '}';
-    }
     
 }
--- src/test/java/za/ac/cput/dominicprzygonski_219206414/ExampleTest.java
+++ src/test/java/za/ac/cput/dominicprzygonski_219206414/ExampleTest.java
@@ -5,13 +5,12 @@
  */
 package za.ac.cput.dominicprzygonski_219206414;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Ignore;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -19,74 +18,28 @@ import org.junit.Ignore;
  */
 public class ExampleTest {
     
-    private Example testing1;
-    private Example testing2;
-    private Example testing3;
-    
     public ExampleTest() {
-        
     }
     
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
-        
-       
     }
     
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
     }
     
-    @Before
+    @BeforeEach
     public void setUp() {
-        testing1 = new Example();
-        testing1.setValue1(5);
-        testing1.setValue2(7);
-        
-        testing2 = new Example();
-        testing2.setValue1(7);
-        testing2.setValue2(5);
-        
-        testing3 = testing2;
     }
     
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
-    //Test Object Equality, meant to pass
-    @Test
-    public void testEquality(){
-        assertEquals(testing1.getValue1(), testing2.getValue2());
-    }
-    
-    //Test Object identity, meant to pass
-    @Test
-    public void testIdentity(){
-        assertSame(testing2, testing3);
-    }
-    
-    //Test fail, meant to fail
-    @Test
-    public void testFail(){
-        fail("This is meant ot fail");
-        assertSame(testing2, testing3);
-    }
-    
-    //Test timeout, meant to fail/error
-    @Test (timeout=2000)
-    public void testTimeout(){
-        while(true){
-            
-        }
-    }
-    
-    //Test disabling, meant to be skip
-    @Test
-    @Ignore
-    public void testIgnore(){
-        System.out.println("This will be skipped");
-    }
-
-    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
 }
