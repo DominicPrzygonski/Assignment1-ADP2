b05dfda590e35d3015210467e3ac5a6a3f127a72
Dominic Przygonski <domprzygonski@gmail.com>
2021-04-01 17:46:23 +0200

    DominicPrzygonski_219206414

--- src/test/java/za/ac/cput/dominicprzygonski_219206414/ExampleTest.java
+++ src/test/java/za/ac/cput/dominicprzygonski_219206414/ExampleTest.java
@@ -1,22 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package za.ac.cput.dominicprzygonski_219206414;
 
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
 
 /**
  *
- * @author Dom
+ * @author Dominic przygonski - 219206414
  */
+
+//First run will sometimes timeout and not show the correct errors as their is no target files and it need to create those first so second run will run fine
+
 public class ExampleTest {
+    
+    private Example testing1;
+    private Example testing2;
+    private Example testing3;
     
     public ExampleTest() {
     }
@@ -30,16 +35,51 @@ public class ExampleTest {
     }
     
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws Exception{
+        testing1 = new Example();
+        testing2 = new Example();
+        testing3 = testing2;
+        
+        testing1.setValue1(5);
+        testing1.setValue2(5);
+        
+       
     }
     
     @AfterEach
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    //Test Object Equality, meant to pass
+    @Test
+    public void testEquality(){
+        int ans = testing1.LessThanTen(testing1.getValue1(), testing1.getValue2());
+        assertEquals(10, ans);
+    }
+    
+    //Test Object identity, meant to pass
+    @Test
+    public void testIdentity(){
+        assertSame(testing2, testing3);
+    }
+    
+    //Test fail, meant to fail
+    @Test
+    public void testFail(){
+        fail("This is meant to fail");
+    }
+   
+    //Test timeout, meant to skip
+    @Test
+    @Timeout(1)
+    void testTimeout() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10);
+        }
+    
+    //Desiabled test, meant to skip
+    @Disabled("Disabled test")
+    @Test
+    void testDisable() {
+        assertEquals(testing1, testing2);
+    }
 }
